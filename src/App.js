d2d1abf2973e5af6dce16cc64d809e5758f02c3f
BROCHARD, Xavier Alexandre <xavier-alexandre.brochard@airbus.com>
2021-03-07 13:50:47 +0100

    Add Network visualization using Nivo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Graph from "./components/Graph";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [pkg, setPkg] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ display: "flex" }}>
+          <input type="text" id="pkgInput" />
+          <button
+            onClick={() => setPkg(document.getElementById("pkgInput").value)}
+          >
+            View
+          </button>
+        </div>
+        <Graph pkg={pkg} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Graph.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { API } from "aws-amplify";
+import { ResponsiveNetwork } from "@nivo/network";
+import sampleData from "./sampleData.json";
+
+const Graph = ({ pkg }) => {
+  const [tree, setTree] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = API.get("DepTree", "/tree");
+      setTree(result);
+    };
+    fetchData();
+  }, [pkg]);
+
+  return (
+    <div style={{ height: 500, width: 500 }}>
+      <ResponsiveNetwork
+        links={sampleData.links}
+        nodes={sampleData.nodes}
+        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+        repulsivity={6}
+        iterations={60}
+        nodeColor={function (e) {
+          return e.color;
+        }}
+        nodeBorderWidth={1}
+        nodeBorderColor={{ from: "color", modifiers: [["darker", 0.8]] }}
+        linkThickness={function (e) {
+          return 2 * (2 - e.source.depth);
+        }}
+        motionStiffness={160}
+        motionDamping={12}
+      />
+    </div>
+  );
+};
+
+export default Graph;
